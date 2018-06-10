2a65e7d83698a667d0cee4a656f32a0ca722795a
Tom Counsell <other@tomcounsell.com>
2018-06-10 00:29:04 +0000

    add the magic beans

--- phos/encrypt/on_http.py
+++ phos/encrypt/on_http.py
@@ -15,20 +15,30 @@ def main(event, context):
     file_data = load_file_data_from_s3(input_filename)
 
 
+    filename = request.args.get('filename', default = "", type = str)
+    # load the zip file and unpack
+    file_data = zipfile.ZipFile(load_file_data_from_s3(input_filename))
 
+    output_filenames = []
 
-    # do something magical
-    output_filename = 'my-new-path-name.txt'
+    # iterate through files in zipfile
+    for name in file_data.namelist():
+        data = file_data.read(name)
+        encrypted_data_a = data['photo']
 
-
-
-    # Save file to s3
-    save_file_data_to_s3(file_data, output_filename)
+        # reencrypt for each reencryption key pair
+        # todo: these can run in parallel, not serialized
+        for (rk, pk) in data['proxy_keys']:
+            encrypted_data_b = pre.reencrypt(rk, encrypted_data_a)
+            unique_filename = "{}-{}.d33p".format(hash(encrypted_data_a), hash(pk))
+            # Save file to s3
+            save_file_data_to_s3(encrypted_data_b, unique_filename)
+            output_filenames.append(unique_filename)
 
 
     body = {
-        "input_file":input_filename,
-        "output_file": output_filename
+        "input_file": input_filename,
+        "output_file": output_filenames
     }
     return {
         "statusCode": 200,
